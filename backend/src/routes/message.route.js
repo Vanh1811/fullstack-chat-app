ff5fd183a58c3d9e873654369140b144c3f14fe1
Vanh1811 <anhnguyenviet065@gmail.com>
2025-04-06 15:35:35 +0800

    fixing route file

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,15 @@
-import express from 'express'
-import { protectRoute } from '../middleware/auth.middleware.js'
-import { getMessages, getUsersForSidebar, sendMessage } from '../controllers/message.controller.js'
+import express from "express";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/users', protectRoute, getUsersForSidebar)
-router.get('/:id', protectRoute, getMessages)
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.post('/send/:id', protectRoute, sendMessage)
-export default router
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
+
+export default router;
