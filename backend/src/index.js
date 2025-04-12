fb44a6596b837214a0428eab0966afb3843cf4d1
Vanh1811 <anhnguyenviet065@gmail.com>
2025-04-12 18:23:31 +0800

    update payload image

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,21 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000; // Fallback port
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" })); // Increase payload limit to 50MB
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // Optional: for URL-encoded data
 app.use(cookieParser());
 app.use(
   cors({
@@ -31,12 +30,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*name", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("Server is running on PORT: " + PORT);
   connectDB();
 });
